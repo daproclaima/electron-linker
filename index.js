b1a2186ca6ceecf490103103755369353209a660
daproclaima <sn.itpro@gmail.com>
2020-06-26 00:41:39 +0200

    creates a window

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const { app } = require('electron')
+const {
+  app,
+  BrowserWindow
+} = require('electron')
+let win
+
+app.on('ready', () => {
+  win = new BrowserWindow({
+    width: 1080,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  return win
+})
